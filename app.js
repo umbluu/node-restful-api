734ae24398a788036b0fa321f518e140f4027772
umbluu <andrep615@hotmail.com>
2018-09-24 14:16:10 +0300

    Added a logger (morgan), nodemon and error handling

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+// Routes which should handle requests
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+module.exports = app;
